c652ff5a2dbb5caad4e2fd9ff62126c1704fbb17
Amuxix <7691673+Amuxix@users.noreply.github.com>
2024-06-07 19:56:34 +0100

    Revert some delays

--- mcoding/keymap.c
+++ mcoding/keymap.c
@@ -384,7 +384,7 @@ void dance_0_finished(tap_dance_state_t *state, void *user_data) {
 }
 
 void dance_0_reset(tap_dance_state_t *state, void *user_data) {
-    wait_ms(5);
+    wait_ms(10);
     switch (dance_state[0].step) {
         case SINGLE_TAP: unregister_code16(LCTL(KC_C)); break;
         case SINGLE_HOLD: unregister_code16(LCTL(LSFT(KC_C))); break;
@@ -420,7 +420,7 @@ void dance_1_finished(tap_dance_state_t *state, void *user_data) {
 }
 
 void dance_1_reset(tap_dance_state_t *state, void *user_data) {
-    wait_ms(5);
+    wait_ms(10);
     switch (dance_state[1].step) {
         case SINGLE_TAP: unregister_code16(LCTL(KC_V)); break;
         case SINGLE_HOLD: unregister_code16(LCTL(LSFT(KC_V))); break;
@@ -454,7 +454,7 @@ void dance_2_finished(tap_dance_state_t *state, void *user_data) {
 }
 
 void dance_2_reset(tap_dance_state_t *state, void *user_data) {
-    wait_ms(5);
+    wait_ms(10);
     switch (dance_state[2].step) {
         case SINGLE_TAP: unregister_code16(LCTL(KC_BSLS)); break;
         case DOUBLE_TAP: unregister_code16(LCTL(LSFT(KC_BSLS))); break;
@@ -488,7 +488,7 @@ void dance_3_finished(tap_dance_state_t *state, void *user_data) {
 }
 
 void dance_3_reset(tap_dance_state_t *state, void *user_data) {
-    wait_ms(5);
+    wait_ms(10);
     switch (dance_state[3].step) {
         case SINGLE_TAP: unregister_code16(LCTL(KC_Z)); break;
         case SINGLE_HOLD: unregister_code16(LCTL(LSFT(KC_Z))); break;
@@ -523,7 +523,7 @@ void dance_4_finished(tap_dance_state_t *state, void *user_data) {
 }
 
 void dance_4_reset(tap_dance_state_t *state, void *user_data) {
-    wait_ms(5);
+    wait_ms(10);
     switch (dance_state[4].step) {
         case SINGLE_TAP: unregister_code16(LCTL(KC_Y)); break;
         case SINGLE_HOLD: unregister_code16(LCTL(LSFT(KC_Y))); break;
@@ -558,7 +558,7 @@ void dance_5_finished(tap_dance_state_t *state, void *user_data) {
 }
 
 void dance_5_reset(tap_dance_state_t *state, void *user_data) {
-    wait_ms(5);
+    wait_ms(10);
     switch (dance_state[5].step) {
         case SINGLE_TAP: unregister_code16(LALT(KC_SPACE)); break;
         case SINGLE_HOLD: unregister_code16(KC_MEDIA_PLAY_PAUSE); break;
@@ -578,7 +578,7 @@ void dance_6_finished(tap_dance_state_t *state, void *user_data) {
 }
 
 void dance_6_reset(tap_dance_state_t *state, void *user_data) {
-    wait_ms(5);
+    wait_ms(10);
     switch (dance_state[6].step) {
     }
     dance_state[6].step = 0;
@@ -594,7 +594,7 @@ void dance_7_finished(tap_dance_state_t *state, void *user_data) {
 }
 
 void dance_7_reset(tap_dance_state_t *state, void *user_data) {
-    wait_ms(5);
+    wait_ms(10);
     switch (dance_state[7].step) {
               case DOUBLE_HOLD:
                 if(!is_layer_locked(5)) {
@@ -630,7 +630,7 @@ void dance_8_finished(tap_dance_state_t *state, void *user_data) {
 }
 
 void dance_8_reset(tap_dance_state_t *state, void *user_data) {
-    wait_ms(5);
+    wait_ms(10);
     switch (dance_state[8].step) {
         case SINGLE_TAP: unregister_code16(KC_LCBR); break;
         case SINGLE_HOLD: unregister_code16(KC_LEFT_ALT); break;
@@ -665,7 +665,7 @@ void dance_9_finished(tap_dance_state_t *state, void *user_data) {
 }
 
 void dance_9_reset(tap_dance_state_t *state, void *user_data) {
-    wait_ms(5);
+    wait_ms(10);
     switch (dance_state[9].step) {
         case SINGLE_TAP: unregister_code16(KC_LPRN); break;
         case SINGLE_HOLD: unregister_code16(KC_LEFT_CTRL); break;
