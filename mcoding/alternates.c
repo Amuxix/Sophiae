fcfd943d1ee74bbf317d28d944a533d012970561
Amuxix <7691673+Amuxix@users.noreply.github.com>
2024-06-19 11:44:34 +0100

    Fix alternate modifiers add backspace as space alternate

--- mcoding/alternates.c
+++ mcoding/alternates.c
@@ -1,6 +1,9 @@
+//In this file even though it it might seem to only check for left modifiers the way they are bit packed
+//it actually checks for both as Right modifiers just has an extra bit set to 1
+//see modifiers.h for more info
 uint16_t get_alt_repeat_key_keycode_user(uint16_t keycode, uint8_t mods) {
   //remove defaults I don't want
-  if ((mods & (MOD_MASK_CTRL | MOD_MASK_ALT | MOD_MASK_GUI))) {
+  if ((mods & (MOD_LCTL | MOD_LALT | MOD_LGUI))) {
     switch (keycode) {
       case KC_F: return KC_NO;
       case KC_D: return KC_NO;
@@ -10,14 +13,14 @@ uint16_t get_alt_repeat_key_keycode_user(uint16_t keycode, uint8_t mods) {
     }
   }
 
-  if ((mods & MOD_MASK_CTRL)) {
+  if ((mods & MOD_LCTL)) {
     switch (keycode) {
       case KC_Y: return LCTL(KC_Z);
       case KC_Z: return LCTL(KC_Y);
     }
   }
 
-  bool shifted = (mods & MOD_MASK_SHIFT);
+  bool shifted = (mods & MOD_LCTL);
   switch (keycode) {
     case KC_TAB:
     if (shifted) {
@@ -35,6 +38,8 @@ uint16_t get_alt_repeat_key_keycode_user(uint16_t keycode, uint8_t mods) {
     case KC_U: return KC_I;
     case KC_R: return KC_L;
     case KC_P: return KC_T;
+
+    case KC_SPACE: return KC_BACKSPACE;
   }
   return KC_TRNS;
 }
