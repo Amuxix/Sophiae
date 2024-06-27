2850f5f4b603a7776fa1386c90eae8b251b0b98e
Amuxix <sofia.martins@hivemindtechnologies.com>
2024-06-27 13:46:37 +0100

    Reduce macro delay, increase tapping term

--- mcoding/macros.c
+++ mcoding/macros.c
@@ -28,57 +28,57 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
     case AO:
       if (record->event.pressed) {
-        SEND_STRING_DELAY(SS_LSFT(SS_TAP(X_GRAVE)) SS_TAP(X_A) SS_TAP(X_O), 10);
+        SEND_STRING_DELAY(SS_LSFT(SS_TAP(X_GRAVE)) SS_TAP(X_A) SS_TAP(X_O), 5);
       }
       break;
     case OES:
       if (record->event.pressed) {
-        SEND_STRING_DELAY(SS_LSFT(SS_TAP(X_GRAVE)) SS_TAP(X_O) SS_TAP(X_E) SS_TAP(X_S), 10);
+        SEND_STRING_DELAY(SS_LSFT(SS_TAP(X_GRAVE)) SS_TAP(X_O) SS_TAP(X_E) SS_TAP(X_S), 5);
       }
       break;
     case CAO:
       if (record->event.pressed) {
-        SEND_STRING_DELAY(SS_LALT(SS_LCTL(SS_TAP(X_COMMA))) SS_LSFT(SS_TAP(X_GRAVE)) SS_TAP(X_A) SS_TAP(X_O), 10);
+        SEND_STRING_DELAY(SS_LALT(SS_LCTL(SS_TAP(X_COMMA))) SS_LSFT(SS_TAP(X_GRAVE)) SS_TAP(X_A) SS_TAP(X_O), 5);
       }
       break;
     case COES:
       if (record->event.pressed) {
-        SEND_STRING_DELAY(SS_LALT(SS_LCTL(SS_TAP(X_COMMA))) SS_LSFT(SS_TAP(X_GRAVE)) SS_TAP(X_O) SS_TAP(X_E) SS_TAP(X_S), 10);
+        SEND_STRING_DELAY(SS_LALT(SS_LCTL(SS_TAP(X_COMMA))) SS_LSFT(SS_TAP(X_GRAVE)) SS_TAP(X_O) SS_TAP(X_E) SS_TAP(X_S), 5);
       }
       break;
     case LEFT_ARROW:
       if (record->event.pressed) {
-        SEND_STRING_DELAY(SS_LSFT(SS_TAP(X_COMMA)) SS_TAP(X_MINUS), 10);
+        SEND_STRING_DELAY(SS_LSFT(SS_TAP(X_COMMA)) SS_TAP(X_MINUS), 5);
       }
       break;
     case RIGHT_ARROW:
       if (record->event.pressed) {
-        SEND_STRING_DELAY(SS_TAP(X_MINUS) SS_LSFT(SS_TAP(X_DOT)), 10);
+        SEND_STRING_DELAY(SS_TAP(X_MINUS) SS_LSFT(SS_TAP(X_DOT)), 5);
       }
       break;
     case EQ_RIGHT_ARROW:
       if (record->event.pressed) {
-        SEND_STRING_DELAY(SS_TAP(X_EQUAL) SS_LSFT(SS_TAP(X_DOT)), 10);
+        SEND_STRING_DELAY(SS_TAP(X_EQUAL) SS_LSFT(SS_TAP(X_DOT)), 5);
       }
       break;
     case BOLD:
       if (record->event.pressed) {
-        SEND_STRING_DELAY(SS_LCTL(SS_LSFT(SS_TAP(X_LEFT))) SS_RCTL(SS_TAP(X_B)) SS_TAP(X_RIGHT) SS_LCTL(SS_TAP(X_RIGHT)), 10);
+        SEND_STRING_DELAY(SS_LCTL(SS_LSFT(SS_TAP(X_LEFT))) SS_RCTL(SS_TAP(X_B)) SS_TAP(X_RIGHT) SS_LCTL(SS_TAP(X_RIGHT)), 5);
       }
       break;
     case UNINDENT:
       if (record->event.pressed) {
-        SEND_STRING_DELAY(SS_LSFT(SS_TAP(X_LEFT)) SS_LSFT(SS_TAP(X_TAB)) SS_LSFT(SS_TAP(X_RIGHT)), 10);
+        SEND_STRING_DELAY(SS_LSFT(SS_TAP(X_LEFT)) SS_LSFT(SS_TAP(X_TAB)) SS_LSFT(SS_TAP(X_RIGHT)), 5);
       }
       break;
     case ITALIC:
       if (record->event.pressed) {
-        SEND_STRING_DELAY(SS_LCTL(SS_LSFT(SS_TAP(X_LEFT))) SS_LCTL(SS_TAP(X_I)) SS_TAP(X_RIGHT) SS_LCTL(SS_TAP(X_RIGHT)), 10);
+        SEND_STRING_DELAY(SS_LCTL(SS_LSFT(SS_TAP(X_LEFT))) SS_LCTL(SS_TAP(X_I)) SS_TAP(X_RIGHT) SS_LCTL(SS_TAP(X_RIGHT)), 5);
       }
       break;
     case INDENT:
       if (record->event.pressed) {
-        SEND_STRING_DELAY(SS_LSFT(SS_TAP(X_LEFT)) SS_TAP(X_TAB) SS_LSFT(SS_TAP(X_RIGHT)), 10);
+        SEND_STRING_DELAY(SS_LSFT(SS_TAP(X_LEFT)) SS_TAP(X_TAB) SS_LSFT(SS_TAP(X_RIGHT)), 5);
       }
       break;
   }
