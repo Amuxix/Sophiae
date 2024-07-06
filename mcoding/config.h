0da04dc1e080c63f5268d2537b955488a856462a
Amuxix <7691673+Amuxix@users.noreply.github.com>
2024-07-06 22:50:34 +0100

    Remove ORYX_CONFIGURATOR

--- mcoding/config.h
+++ mcoding/config.h
@@ -2,7 +2,6 @@
   Set any config.h overrides for your specific keymap here.
   See config.h options at https://docs.qmk.fm/#/config_options?id=the-configh-file
 */
-#define ORYX_CONFIGURATOR
 #define TAP_CODE_DELAY 20
 #undef TAPPING_TERM
 #define TAPPING_TERM 130
