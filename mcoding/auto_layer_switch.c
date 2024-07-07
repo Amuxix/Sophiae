658d4b411646ef4b9aad47f11aab3f36aecd864d
Amuxix <sofia.martins@hivemindtechnologies.com>
2024-07-07 15:36:10 +0100

    Use correct format type

--- mcoding/auto_layer_switch.c
+++ mcoding/auto_layer_switch.c
@@ -6,7 +6,7 @@ RGB webhid_leds[RGB_MATRIX_LED_COUNT];
 void raw_hid_receive(uint8_t *data, uint8_t length) {
   if (strstr((char *) data, "LC")) {
     layer_move(data[2]);
-    uprintf("%s Changing to layer", (char *) &data[2]);
+    uprintf("%i Changing to layer", data[2]);
     raw_hid_send(data, length);
     ML_LED_5(true);
     wait_ms(250);
