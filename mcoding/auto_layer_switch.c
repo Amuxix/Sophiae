1e6cce731b3314ba80051e90c4cdaf11528980b0
Amuxix <sofia.martins@hivemindtechnologies.com>
2024-07-07 16:02:09 +0100

    Fix Ident Use switch

--- mcoding/auto_layer_switch.c
+++ mcoding/auto_layer_switch.c
@@ -5,7 +5,6 @@ RGB webhid_leds[RGB_MATRIX_LED_COUNT];
 
 void raw_hid_receive(uint8_t *data, uint8_t length) {
   if (strstr((char *) data, "LC")) {
-    layer_move(data[2]);
     uprintf("%i Changing to layer", data[2]);
     raw_hid_send(data, length);
     ML_LED_5(true);
@@ -15,6 +14,35 @@ void raw_hid_receive(uint8_t *data, uint8_t length) {
     ML_LED_5(true);
     wait_ms(250);
     ML_LED_5(false);
+
+    switch (data[2]) {
+      case BASE:
+        layer_move(BASE);
+        break;
+      case CANARY_G:
+        layer_move(CANARY_G);
+        break;
+      case QWERTY_G:
+        layer_move(QWERTY_G);
+        break;
+      case QWERTY_SG:
+        layer_move(QWERTY_SG);
+        break;
+      case KEYPAD_SYMB:
+        layer_move(KEYPAD_SYMB);
+        break;
+      case EXTRA_SYMB:
+        layer_move(EXTRA_SYMB);
+        break;
+      case MOVEMENT:
+        layer_move(MOVEMENT);
+        break;
+      case SHORTCUTS:
+        layer_move(SHORTCUTS);
+        break;
+      default:
+        break;
+    }
   } else {
     char response[RAW_EPSIZE] = "ERROR";
     memset(response + 5, 0, RAW_EPSIZE - 5);
