d50aa8930628c9f89898be414436aa7190f58349
Amuxix <sofia.martins@hivemindtechnologies.com>
2024-07-07 19:00:48 +0100

    Convert from ASCII

--- mcoding/auto_layer_switch.c
+++ mcoding/auto_layer_switch.c
@@ -5,12 +5,14 @@
 RGB webhid_leds[RGB_MATRIX_LED_COUNT];
 
 void raw_hid_receive(uint8_t *data, uint8_t length) {
-  ML_LED_5(true);
-  wait_ms(250);
-  ML_LED_5(false);
-  wait_ms(250);
-  ML_LED_5(true);
-  wait_ms(250);
-  ML_LED_5(false);
-  uprintf("%i Changing to layer\n", data[2]);
+  if (strstr((char *) data, "LC")) {
+      uint8_t layer = data[2] - 48; // Convert from ascii
+      uprintf("Changing to layer %i\n", layer);
+      layer_move(layer);
+      raw_hid_send(data, length);
+    } else {
+      char response[RAW_EPSIZE] = "ERROR";
+      memset(response + 5, 0, RAW_EPSIZE - 5);
+      raw_hid_send((uint8_t *) response, RAW_EPSIZE);
+    }
 }
