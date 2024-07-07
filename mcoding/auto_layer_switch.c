b5f4243e05cd7f55bc1686113527dc85fbb77126
Amuxix <7691673+Amuxix@users.noreply.github.com>
2024-07-07 18:41:06 +0100

    Always blink and print

--- mcoding/auto_layer_switch.c
+++ mcoding/auto_layer_switch.c
@@ -4,24 +4,18 @@
 
 RGB webhid_leds[RGB_MATRIX_LED_COUNT];
 
-uint8_t layer = 0;
-void move_layer(void) {
-  layer_move(layer);
-}
-
 void raw_hid_receive(uint8_t *data, uint8_t length) {
+  ML_LED_5(true);
+  wait_ms(250);
+  ML_LED_5(false);
+  wait_ms(250);
+  ML_LED_5(true);
+  wait_ms(250);
+  ML_LED_5(false);
+  uprintf("%i Changing to layer\n", data[2]);
   if (strstr((char *) data, "LC")) {
-    layer = data[2];
-    move_layer();
-    ML_LED_5(true);
-    wait_ms(250);
-    ML_LED_5(false);
-    wait_ms(250);
-    ML_LED_5(true);
-    wait_ms(250);
-    ML_LED_5(false);
     raw_hid_send(data, length);
-    uprintf("%i Changing to layer", data[2]);
+    layer_move(data[2]);
   } else {
     char response[RAW_EPSIZE] = "ERROR";
     memset(response + 5, 0, RAW_EPSIZE - 5);
