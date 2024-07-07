4fff7f9924407758b2cbaca7c640564eada91732
Amuxix <sofia.martins@hivemindtechnologies.com>
2024-07-07 18:41:32 +0100

    Just blink and print

--- mcoding/auto_layer_switch.c
+++ mcoding/auto_layer_switch.c
@@ -13,12 +13,4 @@ void raw_hid_receive(uint8_t *data, uint8_t length) {
   wait_ms(250);
   ML_LED_5(false);
   uprintf("%i Changing to layer\n", data[2]);
-  if (strstr((char *) data, "LC")) {
-    raw_hid_send(data, length);
-    layer_move(data[2]);
-  } else {
-    char response[RAW_EPSIZE] = "ERROR";
-    memset(response + 5, 0, RAW_EPSIZE - 5);
-    raw_hid_send((uint8_t *) response, RAW_EPSIZE);
-  }
 }
