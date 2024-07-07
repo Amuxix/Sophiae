ad1ec0e16ed33fcbff31f2c71c820fa1d1f82024
Amuxix <sofia.martins@hivemindtechnologies.com>
2024-07-07 18:41:06 +0100

    Always blink and print

--- mcoding/rules.mk
+++ mcoding/rules.mk
@@ -2,6 +2,7 @@
 # See rules at https://docs.qmk.fm/#/config_options?id=the-rulesmk-file
 CONSOLE_ENABLE = yes
 COMMAND_ENABLE = yes
+RAW_ENABLE = yes
 AUDIO_ENABLE = no
 MOUSEKEY_ENABLE = no
 ORYX_ENABLE = no
@@ -14,4 +15,3 @@ COMBO_ENABLE = yes
 LAYER_LOCK_ENABLE = yes
 REPEAT_KEY_ENABLE = yes
 KEY_OVERRIDE_ENABLE = yes
-RAW_ENABLE = yes
