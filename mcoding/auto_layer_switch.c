0d07593b21c347f2714ecf8a5b77a3aa9ff8c467
Amuxix <7691673+Amuxix@users.noreply.github.com>
2024-07-07 16:28:27 +0100

    Deferred

--- mcoding/auto_layer_switch.c
+++ mcoding/auto_layer_switch.c
@@ -3,46 +3,24 @@
 
 RGB webhid_leds[RGB_MATRIX_LED_COUNT];
 
+uint8_t layer = 0;
+void move_layer() {
+  layer_move(layer)
+}
+
 void raw_hid_receive(uint8_t *data, uint8_t length) {
   if (strstr((char *) data, "LC")) {
-    uprintf("%i Changing to layer", data[2]);
+    layer = data[2];
+    move_layer();
+    ML_LED_5(true);
+    wait_ms(250);
+    ML_LED_5(false);
+    wait_ms(250);
+    ML_LED_5(true);
+    wait_ms(250);
+    ML_LED_5(false);
     raw_hid_send(data, length);
-    ML_LED_5(true);
-    wait_ms(250);
-    ML_LED_5(false);
-    wait_ms(250);
-    ML_LED_5(true);
-    wait_ms(250);
-    ML_LED_5(false);
-
-    switch (data[2]) {
-      case BASE:
-        layer_move(BASE);
-        break;
-      case CANARY_G:
-        layer_move(CANARY_G);
-        break;
-      case QWERTY_G:
-        layer_move(QWERTY_G);
-        break;
-      case QWERTY_SG:
-        layer_move(QWERTY_SG);
-        break;
-      case KEYPAD_SYMB:
-        layer_move(KEYPAD_SYMB);
-        break;
-      case EXTRA_SYMB:
-        layer_move(EXTRA_SYMB);
-        break;
-      case MOVEMENT:
-        layer_move(MOVEMENT);
-        break;
-      case SHORTCUTS:
-        layer_move(SHORTCUTS);
-        break;
-      default:
-        break;
-    }
+    uprintf("%i Changing to layer", data[2]);
   } else {
     char response[RAW_EPSIZE] = "ERROR";
     memset(response + 5, 0, RAW_EPSIZE - 5);
