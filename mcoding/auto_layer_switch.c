09097efc9270c95cb942bc89bbbc12a858cb58b3
Amuxix <sofia.martins@hivemindtechnologies.com>
2024-12-12 23:13:57 +0000

    Remove dynamic macro dead code Added Special layer to switch space and backspace

--- mcoding/auto_layer_switch.c
+++ mcoding/auto_layer_switch.c
@@ -4,12 +4,20 @@
 
 RGB webhid_leds[RGB_MATRIX_LED_COUNT];
 
-void auto_layer_set(uint8_t layer) {
+void auto_layer_set(layer_state_t state) {
+  // SPACE_BSPC_SWAP is special
+  uint8_t layer = get_highest_layer(state & ~((layer_state_t)1 << SPACE_BSPC_SWAP));
   if (layer >= BASE && layer <= QWERTY_SG) {
     dprintf("Sending layer set %i\n", layer);
     char response[RAW_EPSIZE] = "LS";
     response[2] = (char)(layer + 48);
     memset(response + 3, 0, RAW_EPSIZE - 3);
+    raw_hid_send((uint8_t *) response, RAW_EPSIZE);
+  } else if (IS_LAYER_ON_STATE(state, SPACE_BSPC_SWAP)) {
+    dprintf("Sending layer special set %i\n", layer);
+    char response[RAW_EPSIZE] = "LSS";
+    response[3] = (char)(layer + 48);
+    memset(response + 4, 0, RAW_EPSIZE - 4);
     raw_hid_send((uint8_t *) response, RAW_EPSIZE);
   }
 }
@@ -26,6 +34,11 @@ void raw_hid_receive(uint8_t *data, uint8_t length) {
     if (layer >= BASE && layer <= QWERTY_SG) {
       dprintf("Changing to layer %i\n", layer);
       layer_move(layer);
+      raw_hid_send(data, length);
+      return;
+    } else if (layer == SPACE_BSPC_SWAP) {
+      dprintf("Turning special layer on %i\n", layer);
+      layer_on(layer);
       raw_hid_send(data, length);
       return;
     }
