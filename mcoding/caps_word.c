ad5fbd5f404dc733be2f1b143a376e9661901f98
Amuxix <sofia.martins@hivemindtechnologies.com>
2025-03-25 23:24:47 +0000

    Dance improvements Add CAPS_WORDS_DANCE

--- mcoding/caps_word.c
+++ mcoding/caps_word.c
@@ -1,16 +1,24 @@
-bool caps_word_press_user(uint16_t keycode) {
-    switch (keycode) {
-        case KC_A ... KC_Z:
-            add_weak_mods(MOD_BIT(KC_LSFT));  // Apply shift to next key.
-            return true;
+bool CAPS_PHRASE_STATE = false;
 
-        case KC_1 ... KC_0:
-        case KC_KP_1 ... KC_KP_0:
-        case KC_BSPC:
-        case KC_DEL:
-        case KC_UNDS:
-            return true;  // Keep Caps Word Active.
-        default:
-            return false;  // Deactivate Caps Word.
-    }
+bool caps_word_press_user(uint16_t keycode) {
+  switch (keycode) {
+    case KC_A ... KC_Z:
+      add_weak_mods(MOD_BIT(KC_LSFT));  // Apply shift to next key.
+      return true;
+
+    case KC_1 ... KC_0:
+    case KC_KP_1 ... KC_KP_0:
+    case KC_BSPC:
+    case KC_DEL:
+    case KC_UNDS:
+      return true;  // Keep Caps Word Active.
+    case KC_SPACE:
+      return CAPS_PHRASE_STATE;
+    default:
+      return false;  // Deactivate Caps Word.
+  }
+}
+
+void caps_word_set_user(bool active) {
+  if (!active) CAPS_PHRASE_STATE = false;
 }
--- mcoding/dances.c
+++ mcoding/dances.c
@@ -1,5 +1,5 @@
 enum tap_dance_codes {
-  COPY_CUT,
+  COPY_CUT = 0,
   PASTE,
   MUTE_UNMUTE,
   UNDO,
@@ -7,19 +7,32 @@ enum tap_dance_codes {
   LAUNCH_PAUSE,
   LALT_LBRACK,
   LCTL_LPAREN,
+  CAPS_WORDS_DANCE,
   MAX_DANCES,
 };
 
 #include "dance_helpers.c" // This needs to be after tap_dance_codes because tap_dance_codes is used in the helpers
 
-SINGLE_TAP__SINGLE_HOLD__DOUBLE_TAP__DOUBLE_HOLD(COPY_CUT, LCTL(KC_C), LCS(KC_C), LCTL(KC_X), LCS(KC_X))
-SINGLE_TAP__SINGLE_HOLD(PASTE, LCTL(KC_V), LCS(KC_V))
+SINGLE_TAP__HOLD__DOUBLE_TAP__TAP_HOLD(COPY_CUT, LCTL(KC_C), LCS(KC_C), LCTL(KC_X), LCS(KC_X))
+SINGLE_TAP__HOLD(PASTE, LCTL(KC_V), LCS(KC_V))
 SINGLE_TAP__DOUBLE_TAP(MUTE_UNMUTE, KC_F16, KC_F17)
-SINGLE_TAP__SINGLE_HOLD(UNDO, LCTL(KC_Z), LCS(KC_Z))
-SINGLE_TAP__SINGLE_HOLD(REDO, LCTL(KC_Y), LCS(KC_Y))
+SINGLE_TAP__HOLD(UNDO, LCTL(KC_Z), LCS(KC_Z))
+SINGLE_TAP__HOLD(REDO, LCTL(KC_Y), LCS(KC_Y))
 SINGLE_TAP__DOUBLE_TAP(LAUNCH_PAUSE, KC_MEDIA_PLAY_PAUSE, LALT(KC_SPACE))
-SINGLE_TAP__SINGLE_HOLD(LALT_LBRACK, KC_LCBR, KC_LEFT_ALT)
-SINGLE_TAP__SINGLE_HOLD(LCTL_LPAREN, KC_LPRN, KC_LEFT_CTRL)
+SINGLE_TAP__HOLD(LALT_LBRACK, KC_LCBR, KC_LEFT_ALT)
+SINGLE_TAP__HOLD(LCTL_LPAREN, KC_LPRN, KC_LEFT_CTRL)
+
+void on_caps_words_dance(tap_dance_state_t *state, void *user_data) {
+  switch (dance_step(state)) {
+    case DOUBLE_TAP:
+    case TAP_HOLD:
+      CAPS_PHRASE_STATE = true;
+    case SINGLE_TAP:
+    case HOLD:
+      caps_word_toggle();
+      break;
+  }
+}
 
 tap_dance_action_t tap_dance_actions[MAX_DANCES] = {
   TAP_DANCE_ADVANCED(COPY_CUT),
@@ -30,4 +43,5 @@ tap_dance_action_t tap_dance_actions[MAX_DANCES] = {
   TAP_DANCE_ADVANCED(LAUNCH_PAUSE),
   TAP_DANCE_ADVANCED(LALT_LBRACK),
   TAP_DANCE_ADVANCED(LCTL_LPAREN),
+  TAP_DANCE_ADVANCED(CAPS_WORDS_DANCE, on_caps_words_dance, NULL),
 };
